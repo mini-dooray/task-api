cf6473ce25e7c5e3422d9c5b7608768bc0278331
sangwoong12 <timeattacks2@naver.com>
2023-06-15 02:24:09 +0900

    [Refactor] : getter 추가

--- src/main/java/com/minidooray/taskapi/tasktag/entity/TaskTag.java
+++ src/main/java/com/minidooray/taskapi/tasktag/entity/TaskTag.java
@@ -4,12 +4,14 @@ import com.minidooray.taskapi.tag.entity.Tag;
 import com.minidooray.taskapi.task.entity.Task;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "task_tag")
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskTag {
