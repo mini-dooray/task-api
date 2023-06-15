aa8e4e1c47279ea098413f7e7c6fdccf504dcf1d
sangwoong12 <timeattacks2@naver.com>
2023-06-15 09:24:19 +0900

    [Feat] : projectMember test 추가

--- src/test/java/com/minidooray/taskapi/projectmember/controller/ProjectMemberControllerTest.java
+++ src/test/java/com/minidooray/taskapi/projectmember/controller/ProjectMemberControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minidooray.taskapi.projectmember.dto.request.RequestProjectMemberCreateDto;
 import com.minidooray.taskapi.projectmember.dto.request.RequestProjectMemberDto;
 import com.minidooray.taskapi.projectmember.entity.ProjectMemberAuthority;
+import com.minidooray.taskapi.projectmember.exception.DuplicateMemberProjectException;
 import com.minidooray.taskapi.projectmember.service.ProjectMemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,14 +44,12 @@ class ProjectMemberControllerTest {
 
         RequestProjectMemberCreateDto dto = new RequestProjectMemberCreateDto();
         ReflectionTestUtils.setField(dto, "memberSeq", 1L);
-
         mockMvc.perform(post("/project/1/projectMember/member")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isCreated())
                 .andDo(print());
     }
-
     @Test
     @DisplayName("addMember : Valid 테스트")
     void addMemberFail() throws Exception {
--- src/test/java/com/minidooray/taskapi/projectmember/repository/ProjectMemberRepositoryTest.java
+++ src/test/java/com/minidooray/taskapi/projectmember/repository/ProjectMemberRepositoryTest.java
@@ -5,12 +5,16 @@ import com.minidooray.taskapi.member.entity.Member;
 import com.minidooray.taskapi.project.entity.Project;
 import com.minidooray.taskapi.projectmember.entity.ProjectMember;
 import com.minidooray.taskapi.projectmember.entity.ProjectMemberAuthority;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,5 +70,48 @@ class ProjectMemberRepositoryTest {
         ProjectMember find = entityManager.find(ProjectMember.class, projectMember.getSeq());
 
         assertThat(find).isNull();
+    }
+    @Test
+    void findAllByProjectSeq(){
+        //given
+        Project project = TestUtils.project();
+        entityManager.persist(project);
+        Member member1 = TestUtils.member();
+        entityManager.persist(member1);
+        ProjectMember projectMember1 = TestUtils.projectMember(ProjectMemberAuthority.ADMIN, project, member1);
+        entityManager.persist(projectMember1);
+
+        Member member2 = TestUtils.member();
+        ReflectionTestUtils.setField(member2,"seq",2L);
+        entityManager.persist(member2);
+        ProjectMember projectMember2 = TestUtils.projectMember(ProjectMemberAuthority.ADMIN, project, member2);
+        entityManager.persist(projectMember2);
+
+        //when
+        List<Member> list = projectMemberRepository.findAllByProjectSeq(project.getSeq());
+
+        //then
+        assertThat(list).hasSize(2);
+        assertThat(list.get(0).getName()).isEqualTo(member1.getName());
+        assertThat(list.get(0).getSeq()).isEqualTo(member1.getSeq());
+
+        assertThat(list.get(1).getName()).isEqualTo(member2.getName());
+        assertThat(list.get(1).getSeq()).isEqualTo(member2.getSeq());
+    }
+    @Test
+    void existsByMemberSeqAndProjectSeq(){
+        //given
+        Project project = TestUtils.project();
+        entityManager.persist(project);
+        Member member1 = TestUtils.member();
+        entityManager.persist(member1);
+        ProjectMember projectMember1 = TestUtils.projectMember(ProjectMemberAuthority.ADMIN, project, member1);
+        entityManager.persist(projectMember1);
+
+        //when
+        boolean check = projectMemberRepository.existsByMemberSeqAndProjectSeq(member1.getSeq(), project.getSeq());
+
+        //then
+        assertThat(check).isTrue();
     }
 }
